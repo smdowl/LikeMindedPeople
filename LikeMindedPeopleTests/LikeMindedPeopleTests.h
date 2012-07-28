9eb666e6fc96734fc8c3b31da27690b6a7b9f21d
Shaun Dowling <shaun_dowling@intuit.com>
2012-07-27 21:55:59 -0700

    Initial Commit

---
+++ LikeMindedPeopleTests/LikeMindedPeopleTests.h
@@ -0,0 +1,13 @@
+//
+//  LikeMindedPeopleTests.h
+//  LikeMindedPeopleTests
+//
+//  Created by Shaun Dowling on 7/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LikeMindedPeopleTests : SenTestCase
+
+@end
---
+++ LikeMindedPeopleTests/LikeMindedPeopleTests.m
@@ -0,0 +1,32 @@
+//
+//  LikeMindedPeopleTests.m
+//  LikeMindedPeopleTests
+//
+//  Created by Shaun Dowling on 7/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LikeMindedPeopleTests.h"
+
+@implementation LikeMindedPeopleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LikeMindedPeopleTests");
+}
+
+@end
