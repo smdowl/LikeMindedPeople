db3149c2c533d24e5efc5c3981600e7a7a3f7575
Tyler Weitzman <tylerweitzman@tweitzman.local>
2012-07-28 11:45:19 -0400

    viewcontrollers

--- LikeMindedPeople/AppDelegate.m
+++ LikeMindedPeople/AppDelegate.m
@@ -17,7 +17,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
+//    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;    
     [self.window makeKeyAndVisible];
     return YES;
