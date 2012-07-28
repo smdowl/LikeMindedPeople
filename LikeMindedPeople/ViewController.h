01ac1f3f5e84f82db1732255cc581334251e0497
Tyler Weitzman <tylerweitzman@tweitzman.local>
2012-07-28 13:37:08 -0400

    permissions

--- LikeMindedPeople/ViewController.h
+++ LikeMindedPeople/ViewController.h
@@ -19,4 +19,5 @@
 }
 -(IBAction)search:(id)sender;
 -(IBAction)updateRadius;
+-(IBAction)showPermissions;
 @end
--- LikeMindedPeople/ViewController.m
+++ LikeMindedPeople/ViewController.m
@@ -15,15 +15,38 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     dm = [DataModel sharedInstance];
-//    [dm.contextCoreConnector checkStatusAndOnEnabled:nil disabled:^(NSError *err) {
-        [dm.contextCoreConnector enableFromViewController:self success:nil failure:^(NSError *err) {
-            //show blank view
-        }];
-//    }];
+
+}
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+
+    [dm.contextCoreConnector checkStatusAndOnEnabled:^(QLContextConnectorPermissions *qp) {
+        
+    }disabled:^(NSError *err) {
+    [dm.contextCoreConnector enableFromViewController:self success:nil failure:nil];
+    }];
+    
 }
 -(IBAction)updateRadius:(id)sender {
     MKCoordinateRegion region = [mapView region];
+
+    double miles = slider.value;
     
+    double scalingFactor = ABS( (cos(2 * M_PI * mapView.centerCoordinate.latitude / 360.0) ));
+    
+    MKCoordinateSpan span; 
+    
+    span.latitudeDelta = miles/69.0;
+    span.longitudeDelta = miles/(scalingFactor * 69.0); 
+    
+    region.span = span;
+    region.center = mapView.centerCoordinate;
+    
+    [mapView setRegion:region animated:YES];    
+    
+}
+-(IBAction)showPermissions {
+    [dm.contextCoreConnector showPermissionsFromViewController:self];
 }
 -(IBAction)search:(id)sender {
     [dm.contextCoreConnector enableFromViewController:self success:nil failure:^(NSError *err) {
